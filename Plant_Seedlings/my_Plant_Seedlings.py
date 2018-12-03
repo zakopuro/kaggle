17ab87ce83df089d920518575cf3c71b4c5b9679
zakopuro <zakopuro@gmail.com>
2018-12-03 23:24:11 +0900

    xceptionモデルの理解、numpy,kerasありがとう

--- Plant_Seedlings/my_Plant_Seedlings.py
+++ Plant_Seedlings/my_Plant_Seedlings.py
@@ -65,16 +65,51 @@ def read_img(filepath,size):
 # fig = plt.figure(1,figsize=(NUM_CATEGORIES,NUM_CATEGORIES))
 fig = plt.figure(1,figsize=(5,5))
 grid = ImageGrid(fig,111,nrows_ncols=(NUM_CATEGORIES,NUM_CATEGORIES),axes_pad=0.05)
-# i = 0
-# for category_id, category in enumerate(CATEGORIES):
-#     for filepath in train[train['category'] == category]['file'].values[:NUM_CATEGORIES]:
-#         ax = grid[i]
-#         img = read_img(filepath, (224, 224))
-#         ax.imshow(img / 255.)
-#         ax.axis('off')
-#         if i % NUM_CATEGORIES == NUM_CATEGORIES - 1:
-#             ax.text(250, 112, filepath.split('/')[1], verticalalignment='center')
-#         i += 1
-# plt.show();
+i = 0
+for category_id, category in enumerate(CATEGORIES):
+    for filepath in train[train['category'] == category]['file'].values[:NUM_CATEGORIES]:
+        ax = grid[i]
+        img = read_img(filepath, (224, 224))
+        ax.imshow(img / 255.)
+        ax.axis('off')
+        if i % NUM_CATEGORIES == NUM_CATEGORIES - 1:
+            ax.text(250, 112, filepath.split('/')[1], verticalalignment='center')
+        i += 1
+plt.show();
+
+#%%
+np.random.seed(seed=SEED)
+rnd = np.random.random(len(train))
+# 何故２つに分けた？
+train_idx = rnd < 0.8
+valid_idx = rnd >= 0.8
+ytr = train.loc[train_idx, 'category_id'].values
+yv = train.loc[valid_idx, 'category_id'].values
+len(ytr), len(yv)
+
+
+#%%
+# Xceptionに合わせるため
+INPUT_SIZE = 299
+POOLING = 'avg'
+x_train = np.zeros((len(train), INPUT_SIZE, INPUT_SIZE, 3), dtype='float32')
+for i, file in tqdm(enumerate(train['file'])):
+    img = read_img(file, (INPUT_SIZE, INPUT_SIZE))
+	# おまじない？モデルによって画像の正規化、ImageNetデータセットのRGB各チャンネルごとの平均値を引く、などの処理
+    x = xception.preprocess_input(np.expand_dims(img.copy(), axis=0))
+    x_train[i] = x
+print('Train Images shape: {} size: {:,}'.format(x_train.shape, x_train.size))
+
+#%%
+Xtr = x_train[train_idx]
+Xv = x_train[valid_idx]
+print((Xtr.shape, Xv.shape, ytr.shape, yv.shape))
+# xceptionモデル生成
+xception_bottleneck = xception.Xception(weights='imagenet', include_top=False, pooling=POOLING)
+# 予測
+train_x_bf = xception_bottleneck.predict(Xtr, batch_size=32, verbose=1)
+valid_x_bf = xception_bottleneck.predict(Xv, batch_size=32, verbose=1)
+print('Xception train bottleneck features shape: {} size: {:,}'.format(train_x_bf.shape, train_x_bf.size))
+print('Xception valid bottleneck features shape: {} size: {:,}'.format(valid_x_bf.shape, valid_x_bf.size))
 
 #%%
